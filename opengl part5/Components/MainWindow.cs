5a55c2f856f206b22c8be60147b742a90ac5cd62
eowind <eowind@outlook.com>
2017-02-01 16:04:41 +0100

    opengl part5
    code corresponding to the tutorial at
    http://dreamstatecoding.blogspot.com/2017/02/opengl-4-with-opentk-in-c-part-5.html

---
+++ opengl part5/Components/MainWindow.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+
+namespace techdump.opengl.Components
+{
+    public sealed class MainWindow : GameWindow
+    {
+        private readonly string _title;
+        private int _program;
+        private double _time;
+        private List<RenderObject> _renderObjects = new List<RenderObject>(); 
+
+        public MainWindow()
+            : base(750, // initial width
+                500, // initial height
+                GraphicsMode.Default,
+                "",  // initial title
+                GameWindowFlags.Default,
+                DisplayDevice.Default,
+                4, // OpenGL major version
+                5, // OpenGL minor version
+                GraphicsContextFlags.ForwardCompatible)
+        {
+            _title += "dreamstatecoding.blogspot.com: OpenGL Version: " + GL.GetString(StringName.Version);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Vertex[] vertices =
+            {
+                new Vertex(new Vector4(-0.25f, 0.25f, 0.5f, 1-0f), Color4.HotPink),
+                new Vertex(new Vector4( 0.0f, -0.25f, 0.5f, 1-0f), Color4.HotPink),
+                new Vertex(new Vector4( 0.25f, 0.25f, 0.5f, 1-0f), Color4.HotPink), 
+            };
+            _renderObjects.Add(new RenderObject(vertices));
+
+            CursorVisible = true;
+
+            _program = CreateProgram();
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+            GL.PatchParameter(PatchParameterInt.PatchVertices, 3);
+            Closed += OnClosed;
+        }
+        
+        private void OnClosed(object sender, EventArgs eventArgs)
+        {
+            Exit();
+        }
+
+        public override void Exit()
+        {
+            Debug.WriteLine("Exit called");
+            foreach(var obj in _renderObjects)
+                obj.Dispose();
+            GL.DeleteProgram(_program);
+            base.Exit();
+        }
+        
+        private int CreateProgram()
+        {
+            try
+            {
+                var program = GL.CreateProgram();
+                var shaders = new List<int>();
+                shaders.Add(CompileShader(ShaderType.VertexShader, @"Components\Shaders\1Vert\simplePipeVert.c"));
+                shaders.Add(CompileShader(ShaderType.FragmentShader, @"Components\Shaders\5Frag\simplePipeFrag.c"));
+
+                foreach (var shader in shaders)
+                    GL.AttachShader(program, shader);
+                GL.LinkProgram(program);
+                var info = GL.GetProgramInfoLog(program);
+                if (!string.IsNullOrWhiteSpace(info))
+                    throw new Exception($"CompileShaders ProgramLinking had errors: {info}");
+
+                foreach (var shader in shaders)
+                {
+                    GL.DetachShader(program, shader);
+                    GL.DeleteShader(shader);
+                }
+                return program;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+        private int CompileShader(ShaderType type, string path)
+        {
+            var shader = GL.CreateShader(type);
+            var src = File.ReadAllText(path);
+            GL.ShaderSource(shader, src);
+            GL.CompileShader(shader);
+            var info = GL.GetShaderInfoLog(shader);
+            if (!string.IsNullOrWhiteSpace(info))
+                throw new Exception($"CompileShader {type} had errors: {info}");
+            return shader;
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            HandleKeyboard();
+        }
+        private void HandleKeyboard()
+        {
+            var keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            _time += e.Time;
+            Title = $"{_title}: (Vsync: {VSync}) FPS: {1f / e.Time:0}";
+            Color4 backColor;
+            backColor.A = 1.0f;
+            backColor.R = 0.1f;
+            backColor.G = 0.1f;
+            backColor.B = 0.3f;
+            GL.ClearColor(backColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.UseProgram(_program);
+            foreach(var renderObject in _renderObjects)
+                renderObject.Render();
+            SwapBuffers();
+        }
+        
+    }
+}
